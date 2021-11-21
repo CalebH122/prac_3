5165bb1d9687afeb1825464060523d60380d25a7
Caleb <jiale.hou@my.jcu.edu.au>
2021-11-21 15:23:23 +0800

    Refactor password check program to use functions.

--- Password_check.py
+++ Password_check.py
@@ -1,6 +1,11 @@
 def main():
-    password = input("Please enter your password: ")
+    password = get_password()
     print("Your password is all set! {}".format(len(password) * '*'))
+
+
+def get_password():
+    password = input("Please enter your password: ")
+    return password
 
 
 main()
